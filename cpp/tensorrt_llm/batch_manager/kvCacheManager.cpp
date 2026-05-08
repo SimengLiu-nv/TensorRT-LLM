2b3fea528643e36cd9e8cf29f5d17cfd38e94345
Simeng Liu <simengl@login-ptyche02.ptyche.clusters.nvidia.com>
2026-05-08 07:55:19 -0700

    Revert "Skip no-op KV adjustment after token add"
    This reverts commit 51c25296302802f7a9e072529ce8803be58a9d98.

--- cpp/tensorrt_llm/batch_manager/kvCacheManager.cpp
+++ cpp/tensorrt_llm/batch_manager/kvCacheManager.cpp
@@ -4562,12 +4562,6 @@ void KVCacheManager::addToken(RequestIdType requestId, bool detachSwaFrontBlocks
     auto& sequence = getSequence(requestId);
     sequence.addNewTokens(1);
     auto const effectiveDetachSwaFrontBlocks = detachSwaFrontBlocks && !isSwaContextReuseEnabled();
-    auto const isBlockBoundary = (sequence.getNumTokens() - 1) % getTokensPerBlock() == 0;
-    if (!effectiveDetachSwaFrontBlocks && !isBlockBoundary)
-    {
-        return;
-    }
-
     mBlockManager.adjustBlocksIfNeeded(sequence, effectiveDetachSwaFrontBlocks);
 }
 
